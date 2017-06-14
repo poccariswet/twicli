4423124b7b56d0e8b6be09e1cffcb9dff841fa1b
soeyusuke <s1240231@gmail.com>
2017-06-14 23:57:15 +0900

    #add usershow [Action]

---
+++ usershow.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+func usershow(c *cli.Context) error {
+	if c.NArg() != 1 {
+		cli.ShowCommandHelp(c, "usershow")
+		return fmt.Errorf("\ninvalid arguments or multipl words")
+	}
+	text := c.Args()[0]
+	user, _ := api.GetUsersShow(text, nil)
+	fmt.Println(user.Name)
+	fmt.Printf("Follow   :%d\n", user.FriendsCount)
+	fmt.Printf("Follower :%d\n", user.FollowersCount)
+	fmt.Printf("userID   :%d\n", user.Id)
+	fmt.Println(user.Description)
+	fmt.Println("--------------------------------------------------------------")
+	fmt.Println(user.ProfileImageURL)
+	fmt.Println("--------------------------------------------------------------")
+	fmt.Println(user.ProfileBannerURL)
+	fmt.Println("--------------------------------------------------------------")
+	if user.URL == "" {
+		fmt.Println("User dosen't have another URL.")
+	} else {
+		fmt.Println(user.URL)
+	}
+	return nil
+}
