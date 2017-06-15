34c1f8ad42b6e860125a29e125d45b62697061d6
soeyusuke <s1240231@gmail.com>
2017-06-15 19:44:00 +0900

    #change invalid sentence

--- timeline.go
+++ timeline.go
@@ -11,7 +11,6 @@ func timeline(c *cli.Context) error {
 	v := url.Values{}
 	timelines, _ := api.GetHomeTimeline(v) //default's numbers of timeline are 15
 	for _, timeline := range timelines {
-		// fmt.Printf("%3d:", i+1)
 		fmt.Println("[" + timeline.User.Name + "]" + " " +timeline.Text)
 	}
 	return nil
