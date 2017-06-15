b637552ed5d4aee10c4b60d39cff9be83bc08a85
soeyusuke <s1240231@gmail.com>
2017-06-15 14:47:19 +0900

    #add picture save image [Action]

---
+++ saveimage.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func saveimage(c *cli.Context) error {
+	if c.NArg() != 2 {
+		cli.ShowCommandHelp(c, "saveimage")
+		return fmt.Errorf("\ninvalid arguments or multipl words")
+	}
+
+	url := c.Args()[0]
+	imageName := c.Args()[1]
+
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("This URL is not validity.")
+	}
+	defer res.Body.Close()
+
+	picture, err := os.Create(fmt.Sprintf(root+"/%s.jpg", imageName))
+	if err != nil {
+		return fmt.Errorf("failed create image.")
+	} else {
+		fmt.Println("success")
+	}
+
+	defer picture.Close()
+	io.Copy(picture, res.Body)
+
+	return nil
+}
