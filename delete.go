39e572511320907ddd19bd663789d88afd96744a
soeyusuke <s1240231@gmail.com>
2017-06-15 20:04:50 +0900

    #add delete picture [Action]

---
+++ delete.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	pipeline "github.com/mattn/go-pipeline"
+	"github.com/urfave/cli"
+)
+
+func delete(c *cli.Context) error {
+	if c.NArg() != 0 {
+		cli.ShowCommandHelp(c, "delete")
+    fmt.Println("\nInvalid arguments\nYou should $ twicli delete")
+		return nil
+	}
+
+	dirs := Dirplace(root)
+	var files string
+	for _, dir := range dirs {
+		files += fmt.Sprintf("%s\n", dir)
+	}
+
+	imagename, err := pipeline.Output(
+		[]string{"echo", files},    // pictures in the directory.
+		[]string{os.Getenv(fuzzy)}, // fuzzy search
+	)
+	if err != nil {
+		if strings.Contains("exit status 130", err.Error()) {
+			return nil
+		}
+		return fmt.Errorf("cannot start fuzzy-search: %s", err)
+	}
+
+	if err := os.Remove(strings.TrimSpace(string(imagename))); err != nil { //delete file
+		return fmt.Errorf("\ncannot delete image")
+	}
+  fmt.Println("delete success")
+  return nil
+}
